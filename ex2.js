aeeb9ff1a10eff3fe3f826ff19f648e47b8487ea
Luciana Monsynhatti <lucianamonsynhatti@gmail.com>
2018-06-15 19:27:59 -0300

    exercicio2

--- ex2.js
+++ ex2.js
@@ -1,1 +1,16 @@
+let num = "43"
+let resposta = Number(prompt("em que número estou pensando"));
 
+
+    
+    if (resposta < num){
+        alert ("é maior");
+        }
+    else if(resposta > num){
+        alert("é menor");
+     }  
+   else {
+       alert("PARABÉNS!! você acertou");
+    }
+    
+    
