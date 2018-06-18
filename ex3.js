5a3e7f46c78e6bcb9aa3742dd29ae0ea1fdbaaa4
Luciana Monsynhatti <39928881+LucianaMonsynhatti@users.noreply.github.com>
2018-06-18 12:04:30 -0300

    adicionar for

--- ex3.js
+++ ex3.js
@@ -1,3 +1,24 @@
 let tamanho = prompt("Qual o tamanho da escada?")
 
-   
+   // let tamanho = Number(prompt("qual o tamanho da escadinha"));
+// let contador = 0;
+// let escadinha = "";
+// let simb = prompt("Qual o simbolo da escada?");
+
+// while(contador < tamanho){
+//     escadinha = escadinha + simb;
+//     console.log(escadinha);
+//     contador++;
+// }
+
+
+function escada (inicio, fim) {
+    let simb = prompt("Qual o simbolo da escada?");
+    let escadinha = "";
+    for(let i =inicio; i<fim; i++){
+    escadinha = escadinha +simb;
+    console.log(escadinha);
+    
+    }
+    return escada;
+}
